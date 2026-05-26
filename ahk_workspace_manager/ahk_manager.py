b28373a9737ad13821e19a4554af3c6cbcd97594
DaveyDoo <davidsaunders198@gmail.com>
2026-05-26 13:37:03 +0100

    Turned instance into a class

--- ahk_workspace_manager/ahk_manager.py
+++ ahk_workspace_manager/ahk_manager.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-
 from pathlib import Path
-
 from .config import AppConfig, ManagedItem
 from .process_manager import ProcessManager
 
--- ahk_workspace_manager/single_instance.py
+++ ahk_workspace_manager/single_instance.py
@@ -6,43 +6,16 @@ WINDOW_TITLE = "Tray Manager"
 WM_APP_RESTORE_INSTANCE = 0x8001
 
 
-def bring_existing_instance_to_front(window_title: str = WINDOW_TITLE) -> None:
-    try:
-        import win32gui
-    except ImportError:
-        return
-
-    target_hwnd = None
-
-    def enum_handler(hwnd, _):
-        nonlocal target_hwnd
-        try:
-            title = win32gui.GetWindowText(hwnd)
-        except Exception:
-            return True
-
-        if window_title in title and hwnd != 0:
-            target_hwnd = hwnd
-
-        return True
-
-    try:
-        win32gui.EnumWindows(enum_handler, None)
-    except Exception:
-        return
-
-    if not target_hwnd:
-        return
-
-    try:
-        win32gui.PostMessage(target_hwnd, WM_APP_RESTORE_INSTANCE, 0, 0)
-    except Exception:
-        return
-
-
 class SingleInstanceGuard:
-    def __init__(self, mutex_name: str = MUTEX_NAME) -> None:
+    def __init__(
+        self,
+        mutex_name: str = MUTEX_NAME,
+        window_title: str = WINDOW_TITLE,
+        restore_message: int = WM_APP_RESTORE_INSTANCE,
+    ) -> None:
         self.mutex_name = mutex_name
+        self.window_title = window_title
+        self.restore_message = restore_message
         self.handle = None
         self.already_running = False
 
@@ -57,7 +30,7 @@ class SingleInstanceGuard:
         self.handle = win32event.CreateMutex(None, False, self.mutex_name)
         self.already_running = win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS
         if self.already_running:
-            bring_existing_instance_to_front()
+            self.bring_existing_instance_to_front()
         return not self.already_running
 
     def release(self) -> None:
@@ -69,3 +42,36 @@ class SingleInstanceGuard:
             win32api.CloseHandle(self.handle)
         finally:
             self.handle = None
+
+    def bring_existing_instance_to_front(self) -> None:
+        try:
+            import win32gui
+        except ImportError:
+            return
+
+        target_hwnd = None
+
+        def enum_handler(hwnd, _):
+            nonlocal target_hwnd
+            try:
+                title = win32gui.GetWindowText(hwnd)
+            except Exception:
+                return True
+
+            if self.window_title in title and hwnd != 0:
+                target_hwnd = hwnd
+
+            return True
+
+        try:
+            win32gui.EnumWindows(enum_handler, None)
+        except Exception:
+            return
+
+        if not target_hwnd:
+            return
+
+        try:
+            win32gui.PostMessage(target_hwnd, self.restore_message, 0, 0)
+        except Exception:
+            return
